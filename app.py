0c5c17d5611fbdad8a6f8cb27dcaddd58dce0035
sr-up <ncicartier@gmail.com>
2021-04-06 11:16:46 -0400

    minor code changes

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 #  Copyright (c) 2021. fit&healthy 365
+from itertools import chain
 
 from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
@@ -10,7 +11,6 @@ app = Flask(__name__.split('.')[0])
 
 bootstrap = Bootstrap(app)
 
-app.secret_key = 'two penguins fighting with snakes'
 
 app.config['database'] = {'host': '127.0.0.1',
                           'user': 'fit365',
@@ -41,18 +41,26 @@ def fetch_challenge_events(cid: int, uid: int) -> list:
                'WHERE person_id = %s AND challenge_id = %s'
         cursor.execute(_SQL, (uid, cid))
         rows = cursor.fetchall()
-        dates = [d for sl in rows for d in sl]
+        dates = flatten(rows)
         return dates
+
+
+def flatten(not_flat):
+    flat = list(chain.from_iterable(not_flat))
+    return flat
 
 
 @app.route('/calendar', methods=['POST'])
 def challenge_calendar() -> 'html':
-    cid = request.form.get("cid")
-    uid = request.form.get("uid")
+    cid = request.form.get('cid')
+    uid = request.form.get('uid')
+
     challenge_name, challenge_description = fetch_challenge_header(cid)
     dates = fetch_challenge_events(cid, uid)
     points = len(dates)
+
     cal = calender_html(events=dates)
+
     return render_template('index.html',
                            name=challenge_name,
                            description=challenge_description,
